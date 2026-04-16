f8ac1ae275e5872867fae9bf10c5299b202eb3e9
Helmy-Fathy <7elmyfathy@gmail.com>
2026-04-16 13:23:55 +0200

    3. Take products from the list as long as Their UnitPrice is less than $25

--- G-NET-26-Linq-02/Program.cs
+++ G-NET-26-Linq-02/Program.cs
@@ -1,4 +1,7 @@
-﻿using static LINQ.DataSources.Source;
+﻿using LINQ.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+using static LINQ.DataSources.Source;
 namespace G_NET_26_Linq_02
 {
     internal class Program
@@ -8,12 +11,21 @@ namespace G_NET_26_Linq_02
             ////1. Get top 3 most expensive products
             //var result = ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
 
+
             ////2.show page 2 of products, with page size = 5
             //var result = ProductList.Skip(5).Take(5);
             //foreach (var item in result)
             //{
             //    Console.WriteLine(item);
             //}
+
+
+            ////3. Take products from the list as long as Their UnitPrice is less than $25(list is ordered by price).
+            //var result = ProductList.OrderBy(p => p.UnitPrice).TakeWhile(p => p.UnitPrice < 25);
+            //foreach (var item in result)
+            //    Console.WriteLine(item);
+            
+
 
 
         }
