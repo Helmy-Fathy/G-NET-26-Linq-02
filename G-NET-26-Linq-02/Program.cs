ba0f0ab5de4f1bb09343c21e33fe9fe5f0389efa
Helmy-Fathy <7elmyfathy@gmail.com>
2026-04-16 13:14:01 +0200

    /1. Get top 3 most expensive products

--- G-NET-26-Linq-02/Program.cs
+++ G-NET-26-Linq-02/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace G_NET_26_Linq_02
+﻿using static LINQ.DataSources.Source;
+namespace G_NET_26_Linq_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            //1. Get top 3 most expensive products
+            var result = ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+
         }
     }
 }
