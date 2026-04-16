fb56957b0d57bbd5f64f2fe08546b94b1d7112e0
Helmy-Fathy <7elmyfathy@gmail.com>
2026-04-16 13:18:39 +0200

    2.show page 2 of products, with page size = 5

--- G-NET-26-Linq-02/Program.cs
+++ G-NET-26-Linq-02/Program.cs
@@ -5,8 +5,16 @@ namespace G_NET_26_Linq_02
     {
         static void Main(string[] args)
         {
-            //1. Get top 3 most expensive products
-            var result = ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+            ////1. Get top 3 most expensive products
+            //var result = ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+
+            ////2.show page 2 of products, with page size = 5
+            //var result = ProductList.Skip(5).Take(5);
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
 
         }
     }
